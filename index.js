4410138829d57e7805b06fe28053d0f2c281f715
Anderson Garcia <anderson@pointmedia.com.br>
2023-03-04 20:23:24 -0300

    update

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-function calcular() {
-  const staminaAtual = document.getElementById("staminaAtual").value;
-  const staminaDesejada = document.getElementById("staminaDesejada").value;
-  const horas = (staminaDesejada - staminaAtual) * 5 / 60;
-  const resultado = `Sua stamina atingirá ${staminaDesejada} em ${horas.toFixed(1)} horas.`;
-  document.getElementById("resultado").innerHTML = resultado;
+let buttonCalc = elId("calc");
+
+buttonCalc.addEventListener('click', function (e) {
+  let currentStamina = elId("staminaAtual").value,
+    desiredStamina = elId("staminaDesejada").value,
+    aswer = elId("aswer"),
+    curretStaminaNumber = Number(currentStamina),
+    desiredStaminaNumber = Number(desiredStamina);
+  calc(curretStaminaNumber, desiredStaminaNumber, aswer)
+})
+
+
+function calc(number1, number2, aswer) {
+
+  let hourMultiplier = 0
+
+  for (let i = number1; i < number2; i++) {
+    i < 39 ? hourMultiplier += 3 : hourMultiplier += 6
+  }
+
+  aswer.textContent = `to reach this stamina you need to stay offline ${hourMultiplier} hours`
+}
+
+function elId(el) {
+  return document.getElementById(el);
 }
